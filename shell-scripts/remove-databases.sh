b653415cbe432a7989c80c9bd85663e2cd667a5a
Felipe Gustavo Andrzejewski <felipe.andrzejewski@gmail.com>
2021-06-01 23:45:47 -0300

    Correçao

--- shell-scripts/remove-databases.sh
+++ shell-scripts/remove-databases.sh
@@ -4,7 +4,7 @@
 # Author: Felipe Gustavo Andrzejewski <felipe.andrzejewski@gmail.com>
 
 conf="/var/shell-scripts/mysql.cnf"
-DBS="$(mysql --defaults-extra-file=$conf -e 'SHOW DATABASES' | grep -v Database | grep -v information_schema | grep -v performance_schema | grep -v mysql)";
+DBS="$(mysql --defaults-extra-file=$conf -e 'SHOW DATABASES' | grep -v Database | grep -v information_schema | grep -v performance_schema | grep -v mysql | grep -v sys)";
 
 printf "\n";
 for db in $DBS;
