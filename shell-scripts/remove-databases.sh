42ee04730606fe00a5024b72e84e158297557572
Felipe Gustavo Andrzejewski <felipe.andrzejewski@gmail.com>
2021-06-02 00:03:19 -0300

    Ajustes na documentaçao

--- shell-scripts/remove-databases.sh
+++ shell-scripts/remove-databases.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
-# Script to remove all bases.
 #
-# Author: Felipe Gustavo Andrzejewski <felipe.andrzejewski@gmail.com>
+# Autor: Felipe Gustavo Andrzejewski <felipe.andrzejewski@gmail.com>
+# Data: 01/06/2021
+#
+# Script to remove all bases using a docker environment
 
+#=====================================================#
+# Configuration
+#=====================================================#
 conf="/var/shell-scripts/mysql.cnf"
+
+#=====================================================#
+# Connect and bring all bases except the essentials
+#   Options: add essential bases
+#=====================================================#
 DBS="$(mysql --defaults-extra-file=$conf -e 'SHOW DATABASES' | grep -v Database | grep -v information_schema | grep -v performance_schema | grep -v mysql | grep -v sys)";
 
+#=====================================================#
+# Drop all bases
+#=====================================================#
 printf "\n";
 for db in $DBS;
 do
